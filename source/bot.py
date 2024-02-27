162c4a21574198750c7a8641f82e106334b7269d
Christian U <info@cu-tec.de>
2024-02-27 05:46:36 +0100

    Master (#2)
    * ollama image
    
    * add webui
    
    * basic bot files
    
    * add Dockerfile
    
    ---------
    
    Co-authored-by: Chris <christian@ullihome.de>

--- source/bot.py
+++ source/bot.py
@@ -1,3 +1,128 @@
+from init import *
+import pathlib,database,pandas_ta,importlib.util,logging,os,pandas,sqlalchemy.sql.expression,datetime,sys,backtrader,time,aiofiles,random,backtests,os
+import managepaper,processpaper,os,traceback
+logger = logging.getLogger(os.path.splitext(os.path.basename(__file__))[0])
+loop = None
+lastsend = None
+class Portfolio(Config):
+    def __init__(self, room, **kwargs) -> None:
+        super().__init__(room, **kwargs)
+@bot.listener.on_message_event
+async def tell(room, message):
+    try:
+        global servers,lastsend
+        logger.info(str(message))
+        if not message.body.startswith(prefix) and room.member_count==2:
+            message.body = prefix+' '+message.body
+        match = botlib.MessageMatch(room, message, bot, prefix)
+        if (match.is_not_from_this_bot() and match.prefix()):
+            res = await bot.api.async_client.room_typing(room.room_id,True,timeout=30000)
+        tuser = None
+        if match.is_not_from_this_bot() and room.member_count==2:
+            tuser = message.sender
+        if match.command("change-setting"):
+            set_target = None
+            for server in servers:
+                if server.room == room.room_id and server.name == match.args()[1]:
+                    setattr(server,match.args()[2],match.args()[3])
+                    set_target = server
+                    break
+                for apaper in server.papers:
+                    if apaper['isin'] == match.args()[1]:
+                        apaper[match.args()[2]] = match.args()[3]
+                        set_target = apaper
+                        break
+                if set_target:
+                    if tuser:
+                        server.client = tuser
+                await save_servers()
+                await bot.api.send_text_message(room.room_id, 'ok')
+        elif (match.is_not_from_this_bot() and match.prefix())\
+        and match.command("restart"):
+            pf = None
+            for server in servers:
+                if server.room == room.room_id:
+                    pf = server
+            if tuser:
+                pf.client = tuser
+                await save_servers()
+            await bot.api.send_text_message(room.room_id, 'exitting...')
+            os._exit(0)
+    except BaseException as e:
+        logger.error(str(e), exc_info=True)
+        await bot.api.send_text_message(room,str(e))
+    await bot.api.async_client.room_typing(room.room_id,False,0)
+datasources = []
+strategies = []
+connection = None
+try:
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    logger.info('starting event loop...')
+    loop = asyncio.new_event_loop()
+    logger.info('loading config...')
+    with open('data.json', 'r') as f:
+        nservers = json.load(f)
+        for server in nservers:
+          servers.append(Portfolio(server))
+except BaseException as e:
+    logger.error('Failed to read data.json:'+str(e))
+@bot.listener.on_startup
+async def startup(room):
+    global loop,servers,news_task,dates_task
+    loop = asyncio.get_running_loop()
+    await database.Init(loop)
+    loop.create_task(restart_task())
+@bot.listener.on_message_event
+async def bot_help(room, message):
+    bot_help_message = f"""
+    Help Message:
+        prefix: {prefix}
+        commands:
+            change-setting:
+                command: change-setting setting value
+                    settings:
+                      - model
+                      - system
+            help:
+                command: help, ?, h
+                description: display help command
+                """
+    match = botlib.MessageMatch(room, message, bot, prefix)
+    if match.is_not_from_this_bot() and (
+       match.command("help") 
+    or match.command("?") 
+    or match.command("h")):
+        await bot.api.send_text_message(room.room_id, bot_help_message)
+async def main():
+    try:
+        def unhandled_exception(loop, context):
+            msg = context.get("exception", context["message"])
+            logger.error(f"Unhandled exception caught: {msg}")
+            loop.default_exception_handler(context)
+            os._exit(1)
+        loop = asyncio.get_event_loop()
+        loop.set_exception_handler(unhandled_exception)
+        await bot.main()
+    except BaseException as e:
+        logger.error('bot main fails:'+str(e),stack_info=True)
+        os._exit(1)
+processpaper.bot = bot
+processpaper.servers = servers
+processpaper.datasources = datasources
+processpaper.strategies = strategies
+processpaper.save_servers = save_servers
+managepaper.bot = bot
+managepaper.servers = servers
+managepaper.datasources = datasources
+managepaper.strategies = strategies
+managepaper.save_servers = save_servers
+async def restart_task():
+    shutdown_time = datetime.datetime.now().replace(hour=7, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
+    time_until_shutdown = (shutdown_time - datetime.datetime.now()).total_seconds()
+    await asyncio.sleep(time_until_shutdown)
+    os._exit(1)
+asyncio.run(main())
+=======
 import ollama
 ollama.pull('llama2')
 ollama.list()
---
+++ source/init.py
@@ -0,0 +1,96 @@
+import simplematrixbotlib as botlib,yaml,json,logging,asyncio,nio,pathlib,os,concurrent.futures
+configpath = pathlib.Path(__file__).parent
+if not (configpath / 'config.yml').exists():
+    configpath = configpath.parent
+    if not (configpath / 'config.yml').exists():
+        if (configpath / 'data' / 'config.yml').exists():
+            configpath = configpath / 'data'
+        if (configpath.parent / 'data' / 'config.yml').exists():
+            configpath = configpath.parent / 'data'
+os.chdir(str(configpath))
+with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+try: 
+    prefix = config['server']['prefix']
+except:
+    prefix = config['server']['user']
+class FailsafeBot(botlib.Bot):
+    async def main(self):
+        try:
+            await super().main()
+        except BaseException as e: 
+            logging.error(str(e))
+        while True:
+            logging.error('Server disconnected, reconnecting ...')
+            await asyncio.sleep(1)
+            await self.async_client.sync_forever(timeout=30000, full_state=True)
+creds = botlib.Creds(config['server']['url'], config['server']['user'], config['server']['password'])
+bot = FailsafeBot(creds)
+class Config(object):
+    def __init__(self,room,**kwargs) -> None:
+        if isinstance(room, dict):
+            self.__dict__.update(room)
+        else:
+            self.room = room
+            self.__dict__.update(kwargs)
+loop = None
+servers = []
+async def save_servers():
+    def clean_dict(d):
+        cleaned = {}
+        for k, v in d.items():
+            if not k.startswith("_"):
+                if isinstance(v, dict):
+                    cleaned[k] = clean_dict(v)
+                elif isinstance(v, list):
+                    cleaned[k] = [clean_dict(i) if isinstance(i, dict) else i for i in v if not str(i).startswith("_")]
+                else:
+                    cleaned[k] = v
+        return cleaned
+    global servers
+    sservers = []
+    for server in servers:
+        ndict = clean_dict(server.__dict__)
+        sservers.append(ndict)
+    with open('data.json', 'w') as f:
+        json.dump(sservers,f, skipkeys=True, indent=4)
+def is_valid_event(event):
+    events = (nio.RoomMessageFormatted, nio.RedactedEvent)
+    events += (nio.RoomMessageMedia, nio.RoomEncryptedMedia)
+    return isinstance(event, events)
+async def fetch_room_events(
+    client,
+    start_token: str,
+    room,
+    direction,
+    limit
+) -> list:
+    events = []
+    while len(events)<limit:
+        response = await client.room_messages(
+            room.room_id, start_token, limit=10, direction=direction
+        )
+        if len(response.chunk) == 0:
+            break
+        events.extend(event for event in response.chunk if is_valid_event(event))
+        start_token = response.end
+    return events
+async def get_room_events(client, room, limit = 1):
+    sync_resp = await client.sync(
+        full_state=True, sync_filter={"room": {"timeline": {"limit": limit}}}
+    )
+    start_token = sync_resp.rooms.join[room].timeline.prev_batch
+    # Generally, it should only be necessary to fetch back events but,
+    # sometimes depending on the sync, front events need to be fetched
+    # as well.
+    events = await fetch_room_events(client,start_token,bot.api.async_client.rooms[room],nio.MessageDirection.back,limit)
+    return events
+async def run_in_thread(coroutine,sync=False):
+    loop = asyncio.get_running_loop()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Passen Sie die Coroutine an, um sie als Funktion auszuführen
+        if sync:
+            result = await loop.run_in_executor(executor, coroutine)
+        else:
+            result = await loop.run_in_executor(executor, lambda: asyncio.run(coroutine))
+    return result
