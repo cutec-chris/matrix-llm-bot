231e3e8ddfbbc3de250e8b10c3a4d83d7c71a09b
Chris <christian@ullihome.de>
2024-02-27 05:50:55 +0100

    remove unused code

--- source/wol.py
+++ source/wol.py
@@ -1,12 +1,6 @@
 import sys, struct, socket
-
-# Configuration variables
-broadcast = ['192.168.177.255']
 wol_port = 9
-
-
-def WakeOnLan(ethernet_address):
-
+def WakeOnLan(ethernet_address,broadcast):
     # Construct 6 byte hardware address
     add_oct = ethernet_address.split(':')
     if len(add_oct) != 6:
@@ -19,41 +13,11 @@ def WakeOnLan(ethernet_address):
         int(add_oct[3],16),
         int(add_oct[4],16),
         int(add_oct[5],16))
-
     # Build magic packet
-
     msg = b'\xff' * 6 + hwa * 16
-
     # Send packet to broadcast address using UDP port 9
-
     soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
     for i in broadcast:
         soc.sendto(msg,(i,wol_port))
     soc.close()
-
-def wol(*macs):
-    if len(macs) == 0:
-        print("\n*** No computer given to power up\n")
-        print("Use: 'wol computername' or 'wol 00:11:22:33:44:55'")
-    else:
-        for i in macs:
-            if i[0] != '/':
-                if ":" in i:
-                    # Wake up using MAC address
-                    WakeOnLan(i)
-                else:
-                    # Wake up known computers
-                    if i in known_computers:
-                        WakeOnLan(known_computers[i])
-                    else:
-                        print("\n*** Unknown computer " + i + "\n")
-                        quit()
-
-        if len(macs) == 2:
-            print("\nDone! The computer should be up and running in a short while.")
-        else:
-            print("\nDone! The computers should be up and running in a short while.")
-        print
-
-wol('74:56:3c:92:c4:cc')
