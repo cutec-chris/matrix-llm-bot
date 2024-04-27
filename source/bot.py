0a59ebd8e24f9d579c1f59bb727af1867de11c6e
Christian Ulrich <christian@ullihome.de>
2024-04-27 14:16:02 +0200

    also handle unencrypted media

--- source/bot.py
+++ source/bot.py
@@ -164,21 +164,30 @@ async def handle_message_comfui(room,server,message,match):
         await bot.api.send_text_message(room.room_id,str(e))
     await bot.api.async_client.room_typing(room.room_id,False,0)
 @bot.listener.on_custom_event(nio.RoomEncryptedMedia)
-async def file(room,event):
+async def enc_file(room,event):
     try:
         target_folder = configpath / 'files' / room.room_id[1:room.room_id.find(':')-2]
         response = await bot.async_client.download(mxc=event.url)
-        data = response.body
         pathlib.Path(target_folder).mkdir(parents=True,exist_ok=True)
         async with aiofiles.open(str(target_folder / event.body), "wb") as f:
             await f.write(
                 nio.crypto.attachments.decrypt_attachment(
-                    data,
+                    response.body,
                     event.source["content"]["file"]["key"]["k"],
                     event.source["content"]["file"]["hashes"]["sha256"],
                     event.source["content"]["file"]["iv"],
                 )
             )
+    except BaseException as e:
+        logger.error(str(e), exc_info=True)
+@bot.listener.on_custom_event(nio.RoomMessageMedia)
+async def enc_file(room,event):
+    try:
+        target_folder = configpath / 'files' / room.room_id[1:room.room_id.find(':')-2]
+        response = await bot.async_client.download(mxc=event.url)
+        pathlib.Path(target_folder).mkdir(parents=True,exist_ok=True)
+        async with aiofiles.open(str(target_folder / event.body), "wb") as f:
+            await f.write(response.transport_response._body)
     except BaseException as e:
         logger.error(str(e), exc_info=True)
 @bot.listener.on_message_event
